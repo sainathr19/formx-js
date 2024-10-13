002081f57ded39af20cd3816f1010208e0af7c32
sainath <88471927+sainathr19@users.noreply.github.com>
2024-10-13 19:08:23 +0530

    modified exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 // src/index.ts
 export { FormProvider } from "./FormProvider";
 export { default as Button } from "./ui/button/Button";
+export { default as EmailInput } from "./ui/email/EmailInput";
 export { default as PasswordInput } from "./ui/password/PasswordInput";
-export { default as RadioInput } from "./ui/radio/RadioInput";
 export { default as SelectInput } from "./ui/select/SelectInput";
 export { default as TextInput } from "./ui/text/TextInput";
-export { default as TextArea } from "./ui/textarea/TextArea";
 
