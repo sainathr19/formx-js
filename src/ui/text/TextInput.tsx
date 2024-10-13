4b686b29f61779fb7a52b159f8b63183d3ca856d
sainath <88471927+sainathr19@users.noreply.github.com>
2024-10-13 19:32:38 +0530

    Removed label

--- src/ui/text/TextInput.tsx
+++ src/ui/text/TextInput.tsx
@@ -8,18 +8,16 @@ interface Validator {
 interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
   validators?: Validator[];
   required?: boolean;
-  label?: string;
   id: string;
 }
 const TextInput = forwardRef<HTMLInputElement, TextInputProps>(
-  ({ label, id, validators, required, ...props }, ref) => {
+  ({ id, validators, required, ...props }, ref) => {
     const { formValues, registerFeild, handleChange, errors } = useForm();
     useEffect(() => {
       registerFeild(id, "");
     }, []);
     return (
       <div className="flex flex-col justify-start gap-1">
-        {label && <label className="w-max">{label}</label>}
         <input
           className="p-1 border border-slate-400 rounded-md outline-none"
           ref={ref}
