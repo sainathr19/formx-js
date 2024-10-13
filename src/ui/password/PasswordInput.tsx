bc27aa9430d91d56ddc596f51d08ee2fdd5cc4d9
sainath <88471927+sainathr19@users.noreply.github.com>
2024-10-13 21:13:01 +0530

    added show/hide option for passwords

--- src/ui/password/PasswordInput.tsx
+++ src/ui/password/PasswordInput.tsx
@@ -1,10 +1,12 @@
-import { forwardRef, InputHTMLAttributes, useEffect } from "react";
+import { forwardRef, InputHTMLAttributes, useEffect, useState } from "react";
 import ErrorList from "../../ErrorList";
 import { useForm } from "../../FormProvider";
+
 interface Validator {
   validator: (value: string) => boolean;
   error: string;
 }
+
 interface PasswordInputProps extends InputHTMLAttributes<HTMLInputElement> {
   minLength?: number;
   maxLength?: number;
@@ -12,22 +14,35 @@ interface PasswordInputProps extends InputHTMLAttributes<HTMLInputElement> {
   id: string;
   matchWith?: string;
 }
+
 const PasswordInput = forwardRef<HTMLInputElement, PasswordInputProps>(
   ({ minLength, maxLength, matchWith, validators, id, ...props }, ref) => {
     const { formValues, handleChange, registerFeild, errors } = useForm();
+    const [showPassword, setShowPassword] = useState(false);
+
     useEffect(() => {
       registerFeild(id, "");
-    }, []);
+    }, [id, registerFeild]);
+
     return (
       <div className="flex flex-col justify-start gap-1">
-        <input
-          className="p-1 rounded-md border border-slate-400"
-          ref={ref}
-          type="password"
-          {...props}
-          onChange={(e) => handleChange(id, e.target.value)}
-          value={formValues[id] || ""}
-        />
+        <div className="relative rounded-md border border-slate-400 overflow-hidden">
+          <input
+            className="border-none outline-none pr-16 focus:ring-0"
+            ref={ref}
+            type={showPassword ? "text" : "password"}
+            {...props}
+            onChange={(e) => handleChange(id, e.target.value)}
+            value={formValues[id] || ""}
+          />
+          <button
+            type="button"
+            className="absolute right-2 top-1/2 transform -translate-y-1/2 text-sm text-slate-600"
+            onClick={() => setShowPassword((prev) => !prev)}
+          >
+            {showPassword ? "Hide" : "Show"}
+          </button>
+        </div>
         <ErrorList errors={errors[id]} />
       </div>
     );
